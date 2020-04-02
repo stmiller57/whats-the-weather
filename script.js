4d6b602fb809e2844fb0f22faba35a76c2077c65
Reformed Scribe <stmiller57@gmail.com>
2020-04-02 16:45:11 -0400

    Changed first AJAX call to return imperial data for weather

--- script.js
+++ script.js
@@ -1,9 +1,19 @@
-var locale = "Philadelphia";
-var currentConditions = "https://api.openweathermap.org/data/2.5/weather?q=" + locale + "&appid=eeaf132a34d76337ced09557cd619a19";
+var locale = "Quakertown";
+var currentConditions = "https://api.openweathermap.org/data/2.5/weather?q=" + locale + "&units=imperial&appid=eeaf132a34d76337ced09557cd619a19";
 
-$.ajax({
-    url: currentConditions,
-    method: "GET"
-}).then(function (response) {
-    console.log(response);
+$(document).ready(function () {
+
+    $.ajax({
+        url: currentConditions,
+        method: "GET"
+    }).then(function (response) {
+        console.log(response);
+        $("#city").html(response.name);
+        $("#temp").html("Temperature:" + response.main.temp);
+
+    });
 });
+
+// $("#button-addon2").on("click", function () {
+//     localStorage.setItem("city", city);
+// });
