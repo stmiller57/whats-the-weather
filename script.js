5f50393d2fcf1d539577a24ff48a45dfccd57b6a
Reformed Scribe <stmiller57@gmail.com>
2020-04-03 11:49:26 -0400

    Added AJAX call for 5-day forecast and console.logged it

--- script.js
+++ script.js
@@ -4,7 +4,6 @@ $(document).ready(function () {
     $("#search").click(() => {
         var local = $("#city_search").val();
         var currentConditions = "https://api.openweathermap.org/data/2.5/weather?q=" + local + "&units=imperial&appid=eeaf132a34d76337ced09557cd619a19";
-        $("#city_search").empty();
 
         $.ajax({
             url: currentConditions,
@@ -17,13 +16,21 @@ $(document).ready(function () {
             $("#result").append(`<p>Wind speed: ${response.wind.speed} mph</p>`);
             var { lat } = response.coord;
             var { lon } = response.coord;
-            var uvQueryURL = `https://api.openweathermap.org/data/2.5/uvi?lat=${lat}&lon=${lon}&appid=eeaf132a34d76337ced09557cd619a19`;
+            var uvConditions = `https://api.openweathermap.org/data/2.5/uvi?lat=${lat}&lon=${lon}&appid=eeaf132a34d76337ced09557cd619a19`;
             $.ajax({
-                url: uvQueryURL,
+                url: uvConditions,
                 method: "GET"
             }).then(function (response) {
                 console.log(response);
                 $("#result").append(`<p>UV index: ${response.value}</p>`);
+            });
+            var fiveDayForecast = "https://api.openweathermap.org/data/2.5/forecast?q=" + local + "&units=imperial&appid=eeaf132a34d76337ced09557cd619a19";
+            $.ajax({
+                url: fiveDayForecast,
+                method: "GET"
+            }).then(function (response) {
+                console.log(response);
+                // $("#result").append(`<p>UV index: ${response.value}</p>`);
             });
         });
     });
