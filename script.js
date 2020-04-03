0ba6db98a62a35c49ec28aa9cf49d0e6ae494052
Reformed Scribe <stmiller57@gmail.com>
2020-04-03 12:39:51 -0400

    made AJAX call for 5-day forecast and appended temp/humidity data to cards

--- script.js
+++ script.js
@@ -11,8 +11,8 @@ $(document).ready(function () {
         }).then(function (response) {
             console.log(response);
             $("#result").append(`<h3>${response.name}</h3>`);
-            $("#result").append(`<p>Temperature: ${response.main.temp} degrees Fahrenheit</p>`);
-            $("#result").append(`<p>Humidity: ${response.main.humidity} percent</p>`);
+            $("#result").append(`<p>Temperature: ${response.main.temp} °F</p>`);
+            $("#result").append(`<p>Humidity: ${response.main.humidity}%</p>`);
             $("#result").append(`<p>Wind speed: ${response.wind.speed} mph</p>`);
             var { lat } = response.coord;
             var { lon } = response.coord;
@@ -30,7 +30,16 @@ $(document).ready(function () {
                 method: "GET"
             }).then(function (response) {
                 console.log(response);
-                // $("#result").append(`<p>UV index: ${response.value}</p>`);
+                $("#day1").append(`<p>Temperature: ${response.list[0].main.temp} °F`);
+                $("#day1").append(`<p>Humidity: ${response.list[0].main.humidity}%`);
+                $("#day2").append(`<p>Temperature: ${response.list[8].main.temp} °F`);
+                $("#day2").append(`<p>Humidity: ${response.list[8].main.humidity}%`);
+                $("#day3").append(`<p>Temperature: ${response.list[16].main.temp} °F`);
+                $("#day3").append(`<p>Humidity: ${response.list[16].main.humidity}%`);
+                $("#day4").append(`<p>Temperature: ${response.list[24].main.temp} °F`);
+                $("#day4").append(`<p>Humidity: ${response.list[24].main.humidity}%`);
+                $("#day5").append(`<p>Temperature: ${response.list[32].main.temp} °F`);
+                $("#day5").append(`<p>Humidity: ${response.list[32].main.humidity}%`);
             });
         });
     });
