c82bbedf26891b3251465905550427af0e73de53
Reformed Scribe <stmiller57@gmail.com>
2020-04-05 10:08:12 -0400

    Updated CSS and HTML styles throughout

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ $(document).ready(function () {
     console.log(cities);
     if (cities) {
         for (let i = 0; i < cities.length; i++) {
-            $(".list-group").append(`<li>${cities[i]}</li>`);
+            $(".list-group").append(`<p>${cities[i]}</p>`);
 
         }
     }
@@ -37,7 +37,7 @@ $(document).ready(function () {
                 method: "GET"
             }).then(function (response) {
                 console.log(response);
-                $("#result").append(`<p>UV index: ${response.value}</p>`);
+                $("#result").append(`<h6>UV index: ${response.value}</h6>`);
             });
 
             // ajax call for 5-day forecast
@@ -51,8 +51,8 @@ $(document).ready(function () {
                     .add(1, "days")
                     .format("MMMM Do");
                 $("#firstForecast").html(firstForecast);
-                $("#day1").append(`<p>Temperature: ${response.list[1].main.temp} °F`);
-                $("#day1").append(`<p>Humidity: ${response.list[1].main.humidity}%`);
+                $("#day1").append(`<h6>Temperature: ${response.list[1].main.temp} °F</h6>`);
+                $("#day1").append(`<h6>Humidity: ${response.list[1].main.humidity}%</h6>`);
                 var day1Icon = response.list[1].weather[0].icon;
                 var day1URL = `https://openweathermap.org/img/wn/${day1Icon}@2x.png`;
                 $("#day1Icon").attr("src", day1URL);
@@ -60,8 +60,8 @@ $(document).ready(function () {
                     .add(2, "days")
                     .format("MMMM Do");
                 $("#secondForecast").html(secondForecast);
-                $("#day2").append(`<p>Temperature: ${response.list[9].main.temp} °F`);
-                $("#day2").append(`<p>Humidity: ${response.list[9].main.humidity}%`);
+                $("#day2").append(`<h6>Temperature: ${response.list[9].main.temp} °F</h6>`);
+                $("#day2").append(`<h6>Humidity: ${response.list[9].main.humidity}%</h6>`);
                 var day2Icon = response.list[9].weather[0].icon;
                 var day2URL = `https://openweathermap.org/img/wn/${day2Icon}@2x.png`;
                 $("#day2Icon").attr("src", day2URL);
@@ -69,8 +69,8 @@ $(document).ready(function () {
                     .add(3, "days")
                     .format("MMMM Do");
                 $("#thirdForecast").html(thirdForecast);
-                $("#day3").append(`<p>Temperature: ${response.list[17].main.temp} °F`);
-                $("#day3").append(`<p>Humidity: ${response.list[17].main.humidity}%`);
+                $("#day3").append(`<h6>Temperature: ${response.list[17].main.temp} °F</h6>`);
+                $("#day3").append(`<h6>Humidity: ${response.list[17].main.humidity}%</h6>`);
                 var day3Icon = response.list[17].weather[0].icon;
                 var day3URL = `https://openweathermap.org/img/wn/${day3Icon}@2x.png`;
                 $("#day3Icon").attr("src", day3URL);
@@ -78,8 +78,8 @@ $(document).ready(function () {
                     .add(4, "days")
                     .format("MMMM Do");
                 $("#fourthForecast").html(fourthForecast);
-                $("#day4").append(`<p>Temperature: ${response.list[25].main.temp} °F`);
-                $("#day4").append(`<p>Humidity: ${response.list[25].main.humidity}%`);
+                $("#day4").append(`<h6>Temperature: ${response.list[25].main.temp} °F</h6>`);
+                $("#day4").append(`<h6>Humidity: ${response.list[25].main.humidity}%</h6>`);
                 var day4Icon = response.list[25].weather[0].icon;
                 var day4URL = `https://openweathermap.org/img/wn/${day4Icon}@2x.png`;
                 $("#day4Icon").attr("src", day4URL);
@@ -87,8 +87,8 @@ $(document).ready(function () {
                     .add(5, "days")
                     .format("MMMM Do");
                 $("#fifthForecast").html(fifthForecast);
-                $("#day5").append(`<p>Temperature: ${response.list[33].main.temp} °F`);
-                $("#day5").append(`<p>Humidity: ${response.list[33].main.humidity}%`);
+                $("#day5").append(`<h6>Temperature: ${response.list[33].main.temp} °F</h6>`);
+                $("#day5").append(`<p>Humidity: ${response.list[33].main.humidity}%</h6>`);
                 var day5Icon = response.list[33].weather[0].icon;
                 var day5URL = `https://openweathermap.org/img/wn/${day5Icon}@2x.png`;
                 $("#day5Icon").attr("src", day5URL);
@@ -102,7 +102,7 @@ $(document).ready(function () {
         cities.push(city);
         localStorage.setItem("searchedCities", JSON.stringify(cities));
     });
-    $('.list-group').on("click", 'li', function () {
+    $('.list-group').on("click", 'p', function () {
         console.log(this);
         var city = $(this).text();
         console.log(city);
